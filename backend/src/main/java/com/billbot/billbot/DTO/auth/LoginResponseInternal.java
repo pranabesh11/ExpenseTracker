f46f16556aba9b01024e211b672ed76d08a206cb
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2026-07-08 00:39:03 +0530

    new class added for internal login response management

---
+++ backend/src/main/java/com/billbot/billbot/DTO/auth/LoginResponseInternal.java
@@ -0,0 +1,16 @@
+package com.billbot.billbot.DTO.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseInternal {
+    private String accessToken;
+    private String refreshToken;
+    private UserDto userDto;
+}
