8d34f2106f632328e0596757457860a0837a136a
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2026-07-27 23:42:37 +0530

    new file

--- backend/src/main/java/com/billbot/billbot/repository/settings/SettingsFormMetadataRepository.java
+++ backend/src/main/java/com/billbot/billbot/repository/settings/SettingsFormMetadataRepository.java
@@ -1,8 +1,11 @@
 package com.billbot.billbot.repository.settings;
 
+import com.billbot.billbot.DTO.settings.SettingsFormStructure;
 import com.billbot.billbot.entity.settings.SettingsFormMetaData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SettingsFormMetadataRepository extends JpaRepository<SettingsFormMetaData, Long> {
+import java.util.List;
 
+public interface SettingsFormMetadataRepository extends JpaRepository<SettingsFormMetaData, Long> {
+    List<SettingsFormStructure.Option> findAllByFieldId(Long id);
 }
