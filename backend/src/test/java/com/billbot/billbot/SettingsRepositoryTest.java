14c75c393034da656f5a6ba5527802770e4f9393
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2026-07-31 23:49:01 +0530

    testing and debugging working

--- backend/src/test/java/com/billbot/billbot/SettingsRepositoryTest.java
+++ backend/src/test/java/com/billbot/billbot/SettingsRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.billbot.billbot;
 
+import com.billbot.billbot.entity.settings.Settings;
 import com.billbot.billbot.entity.settings.SettingsFormMetaData;
 import com.billbot.billbot.repository.settings.SettingsFormMetadataRepository;
+import com.billbot.billbot.repository.settings.SettingsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,9 +13,13 @@ public class SettingsRepositoryTest {
 
     @Autowired
     private SettingsFormMetadataRepository settingsFormMetadataRepository;
+    @Autowired
+    private SettingsRepository settingsRepository;
 
     @Test
     void testFindByKey(){
-        System.out.println("hello");
+        Long l1 = settingsFormMetadataRepository.findByKey("firstName").getId();
+        Settings existing = settingsRepository.findByUserIdAndFieldId(7l, 1l);  // returning null this is the problem.
+        System.out.println("********************"+existing);
     }
 }
