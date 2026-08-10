35384a0688eb1ef4911e9ce6885a8e055018e9a4
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2026-08-10 23:49:29 +0530

    just a small locator issue

--- backend/src/main/java/com/billbot/billbot/controller/Dashboard.java
+++ backend/src/main/java/com/billbot/billbot/controller/Dashboard.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("dashboard")
+@RequestMapping("/dashboard")
 @RequiredArgsConstructor
 public class Dashboard {
     private final IncomeOrExpenseService incomeOrExpense;
